3f2edf7295d4518073a64255c343e3abbe755cb1
adjsky <igorlfmartins@mail.ru>
2022-07-14 18:22:24 +0300

    Use relatives

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -1,17 +1,15 @@
-import AssetuxPreview from "../../../public/assetux.webp"
-import FlashbackPreview from "../../../public/flashback.webp"
 
 const projects = [
   {
     title: "ASSETUX",
     description: "A fiat-to-crypto exchange build on interoperable DeFi",
-    preview: AssetuxPreview,
+    preview: "/previews/assetux.webp",
     link: "https://assetux.com"
   },
   {
     title: "FlashBack.one",
     description: "A NFT ticketing platform",
-    preview: FlashbackPreview,
+    preview: "/previews/flashback.webp",
     link: "https://flashback.one"
   }
 ]
