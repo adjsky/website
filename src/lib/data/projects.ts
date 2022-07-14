eb4d9fa7418e37e7b175e395abe0971819d7a524
adjsky <igorlfmartins@mail.ru>
2022-07-14 18:06:06 +0300

    Don't use Image integration

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -2,15 +2,13 @@ const projects = [
   {
     title: "ASSETUX",
     description: "A fiat-to-crypto exchange build on interoperable DeFi",
-    preview:
-      "https://adjskyy.vercel.app/_next/image?url=%2Fprojects%2Fassetux.webp&w=1920&q=75",
+    preview: "/assetux.webp",
     link: "https://assetux.com"
   },
   {
     title: "FlashBack.one",
     description: "A NFT ticketing platform",
-    preview:
-      "https://adjskyy.vercel.app/_next/image?url=%2Fprojects%2Fflashback.webp&w=1920&q=75",
+    preview: "/flashback.webp",
     link: "https://flashback.one"
   }
 ]
