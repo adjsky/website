3ed2ffcf29f16fce04b1b32bae50a7855132f6f1
adjsky <igorlfmartins@mail.ru>
2022-07-18 17:15:01 +0300

    Update previews

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -3,13 +3,13 @@ const projects = [
   {
     title: "ASSETUX",
     description: "A fiat-to-crypto exchange build on interoperable DeFi",
-    preview: "/previews/assetux.webp",
+    preview: "/previews/assetux.jpeg",
     link: "https://assetux.com"
   },
   {
     title: "FlashBack.one",
     description: "A NFT ticketing platform",
-    preview: "/previews/flashback.webp",
+    preview: "/previews/flashback.jpeg",
     link: "https://flashback.one"
   }
 ]
