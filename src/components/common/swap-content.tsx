dca91fc211dd2a3d7c55ed69078146510c42ca23
adjsky <igorlfmartins@mail.ru>
2022-07-21 12:22:11 +0300

    Some code reorganization

--- src/components/common/swap-content.tsx
+++ src/components/common/swap-content.tsx
@@ -2,9 +2,41 @@ import * as React from "react"
 import clsx from "clsx"
 
 import shuffleArray from "@/lib/helpers/shuffle-array"
-import useSwap from "@/lib/hooks/use-swap"
+import useToggle from "@/lib/hooks/use-toggle"
 
-import type { UseSwapProps } from "@/lib/hooks/use-swap"
+type UseSwapProps = {
+  content: string[]
+  hideDuration: number
+  swapInterval: number
+}
+
+const useSwap = ({ content, hideDuration, swapInterval }: UseSwapProps) => {
+  const [currentIndex, setCurrentIndex] = React.useState(0)
+  const [hidden, toggleHidden] = useToggle()
+
+  React.useEffect(() => {
+    const interval = setInterval(async () => {
+      toggleHidden()
+
+      await new Promise((resolve) => setTimeout(resolve, hideDuration))
+
+      toggleHidden()
+      setCurrentIndex((prev) => {
+        const next = prev + 1
+
+        if (next > content.length - 1) {
+          return 0
+        }
+
+        return next
+      })
+    }, swapInterval)
+
+    return () => clearInterval(interval)
+  }, [content, hideDuration, swapInterval])
+
+  return { display: content[currentIndex], hidden }
+}
 
 type SwapContentProps = Partial<UseSwapProps> & {
   transitionDuration?: number
