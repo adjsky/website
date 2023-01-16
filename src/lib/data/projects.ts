34c308164f6cb6816661a7ed3e0db13bd7e0b13b
adjsky <igorlfmartins@mail.ru>
2023-01-16 17:49:41 +0300

    remove assetux from projects

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -1,11 +1,5 @@
 const projects = [
   {
-    title: "ASSETUX",
-    description: "Fiat-to-crypto exchange build on interoperable DeFi",
-    preview: "/previews/assetux.jpeg",
-    link: "https://assetux.com"
-  },
-  {
     title: "Evil Cards",
     description: "Online version of the popular card game",
     preview: "/previews/evil-cards.jpg",
