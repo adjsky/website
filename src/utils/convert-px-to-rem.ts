a8e23cecc5bed8a780e888bdf70fbf704014eb4b
Kirill Timchenko <49305219+adjsky@users.noreply.github.com>
2023-12-11 01:47:10 +0300

    make it simple

--- src/utils/convert-px-to-rem.ts
+++
@@ -1,9 +0,0 @@
-function convertPxToRem(px: number) {
-  const documentFontSize = parseFloat(
-    getComputedStyle(document.documentElement).fontSize
-  )
-
-  return px * (1 / documentFontSize)
-}
-
-export default convertPxToRem
--- src/utils/convert-rem-to-px.ts
+++
@@ -1,5 +0,0 @@
-function convertRemToPx(rem: number) {
-  return rem * parseFloat(getComputedStyle(document.documentElement).fontSize)
-}
-
-export default convertRemToPx
